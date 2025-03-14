f469c781f3f3c0c51f991267b1e42e5e59866a43
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-03-14 10:49:31 -0300

    add locale currency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core'; //Define localizacion
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import localeEs from '@angular/common/locales/es-AR';  // Localización de Argentina
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import {  SpinnerInterceptor } from './interceptors/spinner.interceptor';
+import { registerLocaleData } from '@angular/common';
 
+// Registrar la localización de Argentina (o la que necesites)
+registerLocaleData(localeEs, 'es-ES');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,5 +28,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideAnimationsAsync(),
     provideToastr({timeOut: 1500, preventDuplicates: false}),
+    {provide: LOCALE_ID, useValue: "es-ES"},
   ]
 };
